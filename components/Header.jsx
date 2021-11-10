bf96df7056de9010245d7a5b63fea3959fa002c4
saman keighobadi <saman.keighobadi@gmail.com>
2021-11-10 19:20:52 +0330

    Make links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,24 @@
+import Link from 'next/link'
+
 const Header =  () => {
     return(
         <div>
-            Header
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact'>Contact</Link>
+                    </li>
+                    <li>
+                        <Link href='/posts'>Posts</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
