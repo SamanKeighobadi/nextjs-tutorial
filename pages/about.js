2cbb6e4a1eddf47ff555a4e11f1055ec87be8182
saman keighobadi <saman.keighobadi@gmail.com>
2021-11-10 18:44:41 +0330

    publish project

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Duis laborum ut enim tempor qui incididunt ea occaecat in sint. Id
+        commodo exercitation sit qui. Consectetur incididunt proident pariatur
+        nostrud ullamco eiusmod quis sint mollit. Non excepteur in velit eu id
+        do et nisi culpa tempor elit quis aute. Aliqua occaecat id reprehenderit
+        quis. Nisi duis adipisicing sint minim Lorem duis laboris voluptate.
+        Duis consequat aute mollit laborum amet ut.
+      </p>
+
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,21 @@
+const contact = () => {
+  return (
+    <div>
+      <h1>contact</h1>
+      <p>
+        Officia qui irure amet fugiat cupidatat ad veniam dolore. Ipsum ipsum
+        velit mollit ex. Occaecat dolore adipisicing pariatur nostrud nulla quis
+        nulla nisi aliqua cillum pariatur dolor ea. Consectetur elit dolore
+        deserunt in consectetur magna laboris aute amet.
+      </p>
+      <p>
+        Officia qui irure amet fugiat cupidatat ad veniam dolore. Ipsum ipsum
+        velit mollit ex. Occaecat dolore adipisicing pariatur nostrud nulla quis
+        nulla nisi aliqua cillum pariatur dolor ea. Consectetur elit dolore
+        deserunt in consectetur magna laboris aute amet.
+      </p>
+    </div>
+  );
+};
+
+export default contact;
